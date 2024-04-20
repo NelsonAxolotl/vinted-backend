c2f701cfa6c848b2c66aef03ff854b2bf4dd1b5c
NELSON PARAISO <nelsonparaiso@mbp-de-nelson.home>
2024-04-20 15:41:03 +0200

    payment

--- routes/payment.js
+++ routes/payment.js
@@ -9,7 +9,7 @@ router.post("/payment", async (req, res) => {
   console.log(req.body);
   try {
     // on envoie le token a Stripe avec le montant
-    let { status } = await stripe.charges.create({
+    const paymentIntent = await stripe.paymentIntents.create({
       // Montant de la transaction
       amount: (req.body.amount * 100).toFixed(0),
       // Devise de la transaction
@@ -21,7 +21,7 @@ router.post("/payment", async (req, res) => {
     // Le paiement a fonctionné
     // On peut mettre à jour la base de données
     // On renvoie une réponse au client pour afficher un message de statut
-    res.status(201).json("Payment validé");
+    res.json(paymentIntent);
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ message: error.message });
